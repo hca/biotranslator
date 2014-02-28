1bf2150d714cd5a43d4721cd3923dfa968df5f65
hector <hector.condori.alagon@gmail.com>
2014-02-28 19:19:07 +0100

    Commit de prueba

--- src/biotranslator.c
+++ src/biotranslator.c
@@ -6,9 +6,11 @@
 
 #define PROGRAM_NAME "biotranslator"
 
+
 enum
   {
-    GETOPT_HELP_CHAR = (CHAR_MIN - 2), GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
+    GETOPT_HELP_CHAR = (CHAR_MIN - 2), 
+    GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
   };
 
 #define GETOPT_HELP_OPTION_DECL \
@@ -23,12 +25,26 @@ enum
 
 #define case_GETOPT_HELP_CHAR                   \
   case GETOPT_HELP_CHAR:                        \
-  usage (EXIT_SUCCESS);                       \
+  usage(EXIT_SUCCESS);                       \
   break;
 
-static int verbose_flag;
 
 char* program_name = "biotranslator";
+
+static struct option long_options[] =
+  {
+    /* These options set a flag. */
+    { GETOPT_HELP_OPTION_DECL },
+    { GETOPT_VERSION_OPTION_DECL },
+    /* These options don't set a flag.
+       We distinguish them by their indices. */
+    { "from", required_argument, 0, 'f' },
+    { "to", required_argument, 0, 't' },
+    { "dialect", required_argument, 0, 'd' },
+    { 0, 0, 0, 0 } 
+  };
+
+
 
 void
 usage(int status)
@@ -37,6 +53,12 @@ usage(int status)
   exit(status);
 }
 
+int dnacomplement(char *file)
+{
+  
+}
+
+
 int
 main(int argc, char **argv)
 {
@@ -44,20 +66,9 @@ main(int argc, char **argv)
 
   int option_index = 0;
 
-  static struct option long_options[] =
-    {
-      /* These options set a flag. */
-      { "verbose", no_argument, NULL, 'v' },
-      { GETOPT_HELP_OPTION_DECL },
-      { GETOPT_VERSION_OPTION_DECL },
-      /* These options don't set a flag.
-	 We distinguish them by their indices. */
-      { "from", required_argument, 0, 'f' },
-      { "dialect", required_argument, 0, 'd' },
-      { 0, 0, 0, 0 } };
-
-  while ((c = getopt_long(argc, argv, "abc:d:f:", long_options, &option_index))
-	 != 1)
+  
+  while ((c = getopt_long(argc, argv, "t:d:", long_options, NULL))
+	 != -1)
     {
 
       switch (c)
@@ -81,13 +92,9 @@ main(int argc, char **argv)
 
 	case 'f':
 	  printf("option -f with value `%s'\n", optarg);
-	  break;
+	  break;	
 
-	case '?':
-	  /* getopt_long already printed an error message. */
-	  break;
-
-	  case_GETOPT_HELP_CHAR;
+	case_GETOPT_HELP_CHAR;
 
 	default:
 	  usage(EXIT_FAILURE);
@@ -98,10 +105,9 @@ main(int argc, char **argv)
   /* Instead of reporting ‘--verbose’
      and ‘--brief’ as they are encountered,
      we report the final status resulting from them. */
-  if (verbose_flag)
-    puts("verbose flag is set");
 
   /* Print any remaining command line arguments (not options). */
+
   if (optind < argc)
     {
       printf("non-option ARGV-elements: ");
